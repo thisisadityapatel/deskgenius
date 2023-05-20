4344e45bae098a1fceba431bfc3b05aef460e4ba
Aditya <adityakdpatel@gmail.com>
2023-05-20 16:26:37 -0400

    Solved bug for resetting camera

--- src/pages/assemble.js
+++ src/pages/assemble.js
@@ -36,7 +36,7 @@ const assemble = () => {
 
   const resetTableConfig = (event) => {
     event.preventDefault();
-    setSecondaryModel([{"name": "keyboard2", "count": 0}, {"name": "monitor2", "count": 1}, {"name": "mouse2", "count": 2}, {"name": "pc2", "count": 3}, {"name": "legobatman", "count": 4}, {"name": "tablelamp1", "count": 5}])
+    setSecondaryModel([{"name": "keyboard2", "count": 0}, {"name": "monitor2", "count": 1}, {"name": "mouse2", "count": 2}, {"name": "pc2", "count": 3}, {"name": "legobatman", "count": 4}, {"name": "tablelamp1", "count": 5}, {"name": "asus_rog_g15", "count": 6}])
     setNewOtherModels([]);
     setMainTable("desk3");
     setTableBuffer(deskmodels["desk3"].y_buffer);
