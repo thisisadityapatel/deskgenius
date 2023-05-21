8591850d53ce232f7a884419da74208053b18c71
Aditya Patel <adityapatel@Adityas-MacBook.local>
2023-05-21 11:30:22 -0400

    fixed the bug regarding autopositioning

--- src/pages/license.js
+++ src/pages/license.js
@@ -5,7 +5,7 @@ const license = () => {
   return (
     <div>
         <Header />
-        <div className="px-10 pt-40 text-center">
+        <div className="container mx-auto pt-40 text-center">
             <h1 class="mb-4 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">MIT</span> License</h1>
             
             <div className="mb-6 text-gray-500 dark:text-gray-400">
