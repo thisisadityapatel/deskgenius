195df9a4e1b62538221414f192c7c7fbea0e5835
Aditya Patel <adityapatel@Adityas-MacBook.local>
2023-08-23 00:29:04 -0400

    Added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import Header from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />    
+      <Analytics />
     </>
   )
 }
