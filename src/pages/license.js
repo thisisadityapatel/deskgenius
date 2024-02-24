1e2382f9a60c5e82b9a05e22dad494e63664f447
Aditya Patel <adityapatel@Adityas-MacBook-Air.local>
2024-02-24 09:53:10 -0500

    changes with the copyrights format

--- src/pages/license.js
+++ src/pages/license.js
@@ -2,46 +2,47 @@ import React from 'react'
 import Header from '@/components/Header'
 
 const license = () => {
-  return (
-    <div>
-        <Header />
-        <div className="container mx-auto pt-40 text-center">
-            <h1 className="mb-4 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">MIT</span> License</h1>
-            
-            <div className="mb-6 text-gray-500 dark:text-gray-400">
-                Copyright (c) 2023 Aditya Patel
-            </div>
-
-            <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
-
-            <div className="mb-6">
-                <div className="my-6 text-gray-500 dark:text-gray-400">
-                    Permission is hereby granted, free of charge, to any person obtaining a copy
-                    of this software and associated documentation files (the "Software"), to deal
-                    in the Software without restriction, including without limitation the rights
-                    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                    copies of the Software, and to permit persons to whom the Software is
-                    furnished to do so, subject to the following conditions:
+    const copyRightsYear = new Date().getFullYear()
+    return (
+        <div>
+            <Header />
+            <div className="container mx-auto pt-40 text-center">
+                <h1 className="mb-4 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">MIT</span> License</h1>
+                
+                <div className="mb-6 text-gray-500 dark:text-gray-400">
+                    <span className='normal'>Developed by <span style={{textDecoration: "underline"}}><a href="https://www.linkedin.com/in/thisisadityapatel/" target='_blank'>Aditya Patel</a></span> © {copyRightsYear} All Rights Reserved.</span>
                 </div>
 
-                <div className="mb-4 text-gray-500 dark:text-gray-400">
-                    The above copyright notice and this permission notice shall be included in all
-                    copies or substantial portions of the Software.
-                </div>
+                <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
 
-                <div className="mb-4">
-                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                    SOFTWARE.
+                <div className="mb-6">
+                    <div className="my-6 text-gray-500 dark:text-gray-400">
+                        Permission is hereby granted, free of charge, to any person obtaining a copy
+                        of this software and associated documentation files (the "Software"), to deal
+                        in the Software without restriction, including without limitation the rights
+                        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                        copies of the Software, and to permit persons to whom the Software is
+                        furnished to do so, subject to the following conditions:
+                    </div>
+
+                    <div className="mb-4 text-gray-500 dark:text-gray-400">
+                        The above copyright notice and this permission notice shall be included in all
+                        copies or substantial portions of the Software.
+                    </div>
+
+                    <div className="mb-4">
+                        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                        SOFTWARE.
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default license
